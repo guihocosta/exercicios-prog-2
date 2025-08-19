c4a52f58eea219a72825c91e38325d863b9f48d5
guihocosta <guiihocosta@gmail.com>
2025-08-19 08:04:20 -0300

    refactor: adicionar funções de listas que aprendi na aula passada

--- 01_exercicios/aula_1.py
+++ 01_exercicios/aula_1.py
@@ -4,27 +4,31 @@ def regressiva():
 def metade():
     lista = list()
     for i in range(10):
-        lista.append(int(input()) / 2)
+        lista.append(float(input()) / 2)
     
 def leitura(n):
     lista = list()
     for i in range(n):
-        lista.append(int(input()))
+        lista.append(float(input()))
     return lista
 
-def ocorrencias(lista, elem):
-    cont = 0
-    for item in lista:
-        if elem == item:
-            cont += 1
-    return cont
+def ocorrencias(elem, lista):
+    # cont = 0
+    # for item in lista:
+    #     if elem == item:
+    #         cont += 1
+    # return cont 
+    return lista.count(elem)
+
+      # outra forma de fazer
 
 def maximo(lista):
-    maior = lista[0]
-    for num in lista:
-        if num > maior:
-            maior = num
-    return maior
+    # maior = lista[0]
+    # for num in lista:
+    #     if num > maior:
+    #         maior = num
+    # return maior
+    return max(lista)
 
 def posicao_maximo(lista):
     maior_indice = 0
@@ -34,8 +38,9 @@ def posicao_maximo(lista):
     return maior_indice
 
 def inverter(lista):
-    lista_2 = list()
-    for i in range(len(lista)-1, -1, -1):
-        lista_2.append(lista[i])
-    return lista_2
+    # lista_2 = list()
+    # for i in range(len(lista)-1, -1, -1):
+    #     lista_2.append(lista[i])
+    # return lista_2
+    lista.reverse()
 
